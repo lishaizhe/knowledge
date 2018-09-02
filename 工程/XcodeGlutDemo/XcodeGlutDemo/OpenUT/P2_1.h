f0da491556f7b8b21a967e6e58a4fb0587ae6639
李帅哲 <435832656@qq.com>
2018-09-02 18:33:09 +0800

    123

--- 工程/XcodeGlutDemo/XcodeGlutDemo/OpenUT/P2_1.h
+++ 工程/XcodeGlutDemo/XcodeGlutDemo/OpenUT/P2_1.h
@@ -16,24 +16,28 @@
 
 
 GLBatch triangleBatch;
+GLBatch dotBatch;
 GLShaderManager shaderManager;
 
 void ChangeSize(int width, int height){
-    glViewport(0, 0, width, height);
+    glViewport(2, 0, width, height);
 }
 
 void RenderScene(){
     //清除屏幕的颜色
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
-    GLfloat vRed[] = {1.0f, 0.0f, 0.0f, 1.0f};
+    GLfloat vRed[] = {1.0f, 0.0f, 0.0f, 1.0f}; //采用红色填充
     shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vRed);
     triangleBatch.Draw();
+    GLfloat vRed1[] = {0.0f, 1.0f, 0.0f, 1.0f}; //采用红色填充
+    shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vRed1);
+    dotBatch.Draw();
     //根据上方填充的buffer进行绘制
     glutSwapBuffers();
 }
 
 void SetupRC(){
-    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);  //为RGBA, 所以当前背景图变成了蓝色
     shaderManager.InitializeStockShaders();
     //加载多边形
     GLfloat vVerts[] = {-0.5f, 0.0f, 0.0f,
@@ -42,9 +46,15 @@ void SetupRC(){
     triangleBatch.Begin(GL_TRIANGLES, 3);
     triangleBatch.CopyVertexData3f(vVerts);
     triangleBatch.End();
+    
+    GLfloat vDot[] = {-0.8f, -0.8f, 0.0f};
+    dotBatch.Begin(GL_POINTS, 1);
+    dotBatch.CopyVertexData3f(vDot);
+    dotBatch.End();
 }
 
 void initHook(int argc, char * argv[]){
+
     gltSetWorkingDirectory(argv[0]);
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH|GLUT_STENCIL);
@@ -58,7 +68,22 @@ void initHook(int argc, char * argv[]){
         return;
     }
     SetupRC();
+    
     glutMainLoop();
 }
+
+/*
+ 解析：
+ gltSetWorkingDirectory [GLTools的函数--设置当前的工作目录]
+    实际上在windows中是不必要的,因为工作目录默认就是和可执行程序相同的目录,但是MAC OS中,这个程序将当前的工作文件夹改成应用捆绑包(Application Bundle)中的 /Resource 文件夹
+ 
+ glutDisplayFunc()   //显示回调
+ glutReshapeFunc()   //窗口发生变化的回调
+ 
+ 
+ 
+ */
+
+
 
 #endif /* P2_1_h */
