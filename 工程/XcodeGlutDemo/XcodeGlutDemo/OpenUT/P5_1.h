b5d05a49949ae2640be8d4aa055f6b8c46da566f
李帅哲 <435832656@qq.com>
2018-10-31 08:26:28 +0800

    修复工程

---
+++ 工程/XcodeGlutDemo/XcodeGlutDemo/OpenUT/P5_1.h
@@ -0,0 +1,22 @@
+//
+//  P5_1.h
+//  XcodeGlutDemo
+//
+//  Created by 李帅哲 on 2018/10/25.
+//  Copyright © 2018年 李帅哲. All rights reserved.
+//
+
+#ifndef P5_1_h
+#define P5_1_h
+
+/*
+ GL_UNPACK_SWAP_BYTES / GL_PACK_SWAP_BYTES  如果是TRUE，字节反转存储
+ GL_UNPACK_LSB_FIRST / GL_PACK_LSB_FIRST 如果是false，表示从高位开始拍，
+                                        如果是true，表示从低位开始排
+ GL_UNPACK_ROW_LENGTH / GL_PACK_ROW_LENGTH 当从内存读取矩形的图像数据小于内存图像数据矩形实际长度，需要设置这个值进行指定，如果这个值为0，使用glDrawPixels,glCopyPixels,glReadPiexls所指定的宽度值
+ GL_UNPACK_SHIP_ROWS,GL_PACK_SHIP_ROWS 如果从内存读取的子矩形图形数据并不是从0,0开始作为起点，使用这个进行设置起始点（ROW）
+ GL_UNPACK_SHIP_PIXELS,GL_PACK_SHIP_PIXELS 如果从内存读取的子矩形图形数据并不是从0,0开始作为起点，使用这个进行设置起始点（COL）
+ GL_UNPACK_ALIGNMENT,GL_PACK_ALIGNMENT 设置内存对其方式
+ */
+
+#endif /* P5_1_h */
